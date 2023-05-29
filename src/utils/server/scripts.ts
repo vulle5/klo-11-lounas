c5fc35d3a5f70fcd2e672ab4a42569b021b533b1
Severi Tikkanen <15669924+vulle5@users.noreply.github.com>
2023-05-29 19:31:31 +0300

    Refactored code with copilot to make it more readable

--- src/utils/server/scripts.ts
+++ src/utils/server/scripts.ts
@@ -1,14 +1,16 @@
+import { DataMap, DataPath, Menu } from "@prisma/client";
 import { prisma } from "./prisma";
 import { get } from "lodash";
 
-export async function createLocation() {
+type DataMapWithPaths = DataMap & { paths: DataPath[] };
+
+export async function createLocation({ name, dataUrl }: { name: string; dataUrl: string }) {
   return await prisma.location.create({
     data: {
-      name: "Quartetto Plus",
+      name,
       dataMap: {
         create: {
-          dataUrl:
-            "https://www.compass-group.fi/menuapi/feed/json?costNumber=3114&language=fi",
+          dataUrl,
           dataType: "json",
         },
       },
@@ -18,7 +20,6 @@ export async function createLocation() {
 
 export async function findOrCreateMenuForLocation(locationId: number) {
   const today = new Date();
-  today.setHours(0, 0, 0, 0);
 
   // Create a new menu for today if it doesn't exist
   let menu = await prisma.menu.findFirst({
@@ -69,59 +70,40 @@ export async function createMenuItemsForMenu(menuId: number) {
   if (!data) {
     throw new Error("Data not found");
   }
+
   // Find menu items from data
-  const menuItemsPath = dataMap.paths.find(
-    (path) => path.table === "Menu" && path.column === "items"
-  );
-  if (!menuItemsPath) {
-    throw new Error(
-      "Menu items path not found, create dataPath with table: Menu and column: items"
-    );
-  }
+  const menuItemsPath = findDataPath(dataMap, "Menu", "items");
   const menusInData = get(data, menuItemsPath.path);
   if (!Array.isArray(menusInData)) {
     throw new Error("Menus not found from data");
   }
 
   // Find menu item name, price paths and menu date from dataMap
-  const menuItemNamePath = dataMap.paths.find(
-    (path) => path.table === "MenuItem" && path.column === "name"
-  );
-  const menuItemPricePath = dataMap.paths.find(
-    (path) => path.table === "MenuItem" && path.column === "price"
-  );
-  const menuDatePath = dataMap.paths.find(
-    (path) => path.table === "Menu" && path.column === "date"
-  );
-  if (!menuItemNamePath) {
-    throw new Error(
-      "MenuItem name path not found, create dataPath with table: MenuItem and column: name"
-    );
-  }
-  if (!menuItemPricePath) {
-    throw new Error(
-      "MenuItem price path not found, create dataPath with table: MenuItem and column: price"
-    );
-  }
+  const menuItemNamePath = findDataPath(dataMap, "MenuItem", "name");
+  const menuItemPricePath = findDataPath(dataMap,"MenuItem", "price");
+  const menuDatePath = findDataPath(dataMap, "Menu", "date");
+
   const menuInData = menusInData.find((dataMenu) =>
     parseDateFromDateTime(
-      new Date(get(dataMenu, menuDatePath?.path ?? ""))
+      new Date(get(dataMenu, menuDatePath.path))
     ).includes(parseDateFromDateTime(menu.date))
   );
   if (!menuInData) {
     throw new Error(`Menu for date ${menu.date} not found from data`);
   }
-  // Create menu items for menu
-  // Id path is used to check if menu items are an array or a single object
-  // If the name and price are included in the menuInData object, id path is not needed
+
+  // Find the path to the menu items in the dataMap
   const menuItemsIdPath = dataMap.paths.find(
     (path) => path.table === "Menu" && path.column === "id"
   );
+
+  // If the path exists, the menu items are an array
   if (menuItemsIdPath) {
     const menuItemOrItems = get(menuInData, menuItemsIdPath.path);
     if (!Array.isArray(menuItemOrItems)) {
       throw new Error("Menu items not found from data");
     }
+    // Create menu items for each item in the array
     await createMenuItem(
       menuItemOrItems,
       menuItemNamePath,
@@ -129,45 +111,43 @@ export async function createMenuItemsForMenu(menuId: number) {
       menu
     );
   } else {
+    // If the path doesn't exist, the menu items are a single object
     await createMenuItem(menuInData, menuItemNamePath, menuItemPricePath, menu);
   }
 }
 
-// FIXME: Proper array and string handling and price parsing
-async function createMenuItem(
-  menuItemOrItems: any,
-  menuItemNamePath: any,
-  menuItemPricePath: any,
-  menu: any
-) {
-  if (Array.isArray(menuItemOrItems)) {
-    await prisma.menuItem.createMany({
-      data: menuItemOrItems.map((menuItem: any) => ({
-        name: Array.isArray(get(menuItem, menuItemNamePath.path))
-          ? get(menuItem, menuItemNamePath.path).join(", ")
-          : get(menuItem, menuItemNamePath.path),
-        price: get(menuItem, menuItemPricePath.path),
-        menuId: menu.id,
-      })),
-    });
-  } else {
-    if (!menuItemOrItems?.name) {
-      throw new Error("Menu item name not found from data");
-    }
+function findDataPath(dataMap: DataMapWithPaths, table: string, column: string): DataPath {
+  const path = dataMap.paths.find((path) => path.table === table && path.column === column);
+  if (!path) {
+    throw new Error(`DataPath not found for table: ${table}, column: ${column}`);
+  }
+  return path;
+}
 
-    await prisma.menuItem.create({
-      data: {
-        name: Array.isArray(get(menuItemOrItems, menuItemNamePath.path))
-          ? get(menuItemOrItems, menuItemNamePath.path).join(", ")
-          : get(menuItemOrItems, menuItemNamePath.path),
-        price: get(menuItemOrItems, menuItemPricePath.path),
-        menu: {
-          connect: {
-            id: menu.id,
-          },
-        },
-      },
-    });
+async function createMenuItem(
+  menuItemOrItems: any[],
+  menuItemNamePath: DataPath,
+  menuItemPricePath: DataPath,
+  menu: Menu
+) {
+  // Transform menu item or items to array
+  const menuItems = Array.isArray(menuItemOrItems) ? menuItemOrItems : [menuItemOrItems];
+
+  // Create menu item objects for prisma 
+  const data = menuItems.map((menuItem) => {
+    const name = Array.isArray(get(menuItem, menuItemNamePath.path))
+      ? get(menuItem, menuItemNamePath.path).join(", ")
+      : get(menuItem, menuItemNamePath.path);
+
+    const price = get(menuItem, menuItemPricePath.path);
+
+    return { name, price, menuId: menu.id };
+  }).filter((menuItem) => menuItem.name);
+
+  if (data.length > 1) {
+    await prisma.menuItem.createMany({ data });
+  } else {
+    await prisma.menuItem.create({ data: data[0] });
   }
 }
 
