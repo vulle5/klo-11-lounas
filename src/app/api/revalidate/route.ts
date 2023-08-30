1e53ce369923dd5c53ac783fef04b6cf0402ba12
Severi Tikkanen <15669924+vulle5@users.noreply.github.com>
2023-08-30 20:03:21 +0300

    Testing ridiculous workaround

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -6,5 +6,6 @@ export const dynamic = "force-dynamic";
 // Syncs the data from the dataUrl to the database
 export async function GET(request: NextRequest) {
   revalidatePath("/hello");
+  await fetch(`https://${request.headers.get("Host")}/hello`);
   return NextResponse.json({ revalidated: true, date: new Date() });
 }
