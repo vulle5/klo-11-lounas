1264aebcf8cedb21ad987cca026dc35294021e19
Severi Tikkanen <15669924+vulle5@users.noreply.github.com>
2023-09-19 21:18:03 +0300

    More debugging

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,13 +25,14 @@ export function middleware(request: NextRequest) {
     const redirectUrl = new URL(request.url);
     redirectUrl.hostname = 'klo-11-lounas.vercel.app';
     redirectUrl.searchParams.set('redirected', 'true');
+    console.log(redirectUrl.toString());
 
     // Clone the headers to a new object and set the X-Redirected-From header
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set('x-redirected-from', requestUrl.hostname);
 
     return NextResponse.redirect(redirectUrl, {
-      status: 308,
+      status: 302,
       headers: requestHeaders,
     });
   }
