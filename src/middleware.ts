7c92c0e1980c3b8d98101993048392611ec58540
Severi Tikkanen <15669924+vulle5@users.noreply.github.com>
2024-09-18 20:32:34 +0300

    Trying to simplify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,35 +2,26 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const requestUrlObject = new URL(request.url);
-
   // Secure the cron endpoints
   if (
-    requestUrlObject.pathname.startsWith('/api/cron') &&
+    request.nextUrl.pathname.startsWith('/api/cron') &&
     request.headers.get('authorization') !== `Bearer ${process.env.CRON_SECRET}`
   ) {
     return NextResponse.json({ error: 'Unauthorized cron invocation' }, { status: 401 });
   }
 
-  // Use fetch to run vercel cron jobs in production url
-  // Redirects do not work in vercel cron jobs
-  // TODO: Probably caused by bug in Vercel, remove when fixed
+  // Redirect vercel cron jobs to production url
   if (
     request.headers.get('user-agent') === 'vercel-cron/1.0' &&
-    requestUrlObject.hostname !== 'klo-11-lounas.vercel.app'
+    request.nextUrl.hostname !== 'klo-11-lounas.vercel.app'
   ) {
     console.log('Running vercel cron job, fetching the production url...');
     const redirectUrl = new URL(request.url);
     redirectUrl.hostname = 'klo-11-lounas.vercel.app';
 
-    await fetch(redirectUrl.toString(), {
-      headers: {
-        'User-Agent': 'vercel-cron/1.0',
-      },
-    });
+    console.log('Redirecting to:', redirectUrl.toString());
+    return NextResponse.redirect(redirectUrl);
   }
-
-  return NextResponse.next();
 }
 
 export const config = {
